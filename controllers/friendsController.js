3cb17447ff00bdfe7bf43438c964cfed65a91012
Anas Ryadi <anasryadi0@gmail.com>
2022-06-29 14:08:00 -0700

    created a get all friends plans and a get a single plan in friendsController.js

--- controllers/friendsController.js
+++ controllers/friendsController.js
@@ -1,2 +1,15 @@
-const express = require('express')
-const Friends = require("../models/Friends")
+const express = require("express");
+const Friends = require("../models/Friends");
+const router = express.Router();
+
+// Get all friends
+router.get("/", (req, res) => {
+  Friends.find({}).then((friends) =>
+    res.status(200).json({ friends: friends })
+  );
+});
+// Get single plan /friends/:plan
+router.get('/:plan', (req, res) => {
+    Friends.find({Plan: req.params.plan})
+    .then(plan => res.status(200).json({plan: plan}))
+})
