4fe17bf9e7083c48a0f78a496d06a0709ae0944a
Anas Ryadi <anasryadi0@gmail.com>
2022-06-28 23:24:54 -0700

    setting up the port

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
-const express = require('express')
-const app = express()
+// Require dependencies
+const express = require("express");
+const app = express();
+require('dotenv').config()
 
-app.listen(3000)
+// Connections to controller files
+
+// Defining ports
+app.set("PORT", process.env.PORT || 8080);
+
+// app.get("/", (req, res) => {
+//   console.log("Here");
+//   res.render("index");
+// });
+
+
+
+app.listen(app.get("PORT"), () => {
+  console.log(`PORT: ${app.get("PORT")}`);
+});
